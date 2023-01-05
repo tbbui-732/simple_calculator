ef10ab38bde27772cb3e2eafafd8012e9d7dfdc1
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-01-04 21:15:48 -0500

    added readNumberPad and readFunctionKey into main.js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Read number pad
+
+function readNumberPad() {
+    const numpad_key = document.querySelectorAll('#num-pad');
+
+    numpad_key.forEach(function(button) {
+        button.addEventListener('click', function() {
+            console.log(`${this.innerHTML} was clicked`);
+        });
+    });
+};
+
+function readFunctionKeys() {
+    const decimal_key = document.querySelector('#decimal');
+    const negate_key = document.querySelector('#negate');
+    const caret_key = document.querySelector('#caret');
+    const open_bracket = document.querySelector('#open_bracket');
+    const close_bracket = document.querySelector('#close_bracket');
+
+    decimal_key.addEventListener('click', function() {
+        console.log(`${this.innerHTML}was clicked`);
+    });
+
+    negate_key.addEventListener('click', function() {
+        console.log(`${this.innerHTML}was clicked`);
+    });
+
+    caret_key.addEventListener('click', function() {
+        console.log(`${this.innerHTML}was clicked`);
+    });
+
+    open_bracket.addEventListener('click', function() {
+        console.log(`${this.innerHTML}was clicked`);
+    });
+
+    close_bracket.addEventListener('click', function() {
+        console.log(`${this.innerHTML}was clicked`);
+    });
+
+}
+
+// main
+readNumberPad();
+readFunctionKeys();
