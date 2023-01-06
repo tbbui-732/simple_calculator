d37008fd6c062a2e5df13d9eea76d8c98f9a864b
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-01-06 18:44:21 -0500

    removed some events and added functionality to readNumberPad()

--- js/main.js
+++ js/main.js
@@ -1,65 +1,52 @@
-// --- CALCULATOR FUNCTIONALITY ---
-
-function mainCalculate() {
-    readNumberPad()
-    // const number_list = readNumberPad(); 
-    // console.log(number_list);
-    return;
-};
-
-// --- FUNCTIONS TO READ BUTTONS ---
-
 // -------- global variables --------
 // Selecting number pad key elements
 const numpad_key = document.querySelectorAll('#num-pad');
 
 // Selecting function key elements
-const decimal_key = document.querySelector('#decimal');
-const negate_key = document.querySelector('#negate');
-const caret_key = document.querySelector('#caret');
-const open_bracket = document.querySelector('#open_bracket');
-const close_bracket = document.querySelector('#close_bracket');
+// const decimal_key = document.querySelector('#decimal');
+// const negate_key = document.querySelector('#negate');
+// const caret_key = document.querySelector('#caret');
+// const open_bracket = document.querySelector('#open_bracket');
+// const close_bracket = document.querySelector('#close_bracket');
 
 // Selecting operation key elements
-const add = document.querySelector('#add');
-const sub = document.querySelector('#sub');
-const mul = document.querySelector('#mul');
-const div = document.querySelector('#div');
-
+// const add = document.querySelector('#add');
+// const sub = document.querySelector('#sub');
+// const mul = document.querySelector('#mul');
+// const div = document.querySelector('#div');
 function readNumberPad() {
-    // NOTE: del once done, this is for dev purposes
-    let count = 0;
+    const operation_key = document.querySelectorAll('.function');
+    const enter_key = document.querySelector('#enter');
 
     // Reading numpad selections
     let number_list = [];
     numpad_key.forEach(function(button) {
         button.addEventListener('click', function() {
             number_list.push(parseInt(this.innerHTML));
-            count = count + 1;
-            console.log(typeof(parseInt(this.innerHTML))); // delete
+            console.log(typeof(parseInt(this.innerHTML))); // NOTE: del
+        });
+    });
+    
+    // Selecting operation
+    operation_key.forEach(function(button) {
+        button.addEventListener('click', function() {
+            console.log(this.id + " operation has been selected"); // NOTE: del 
+            number_list.push(this.id);
         });
     });
 
-    add.addEventListener('click', function() {
-        // Print counter value
-        console.log(count);
-       
-        // Reset counter
-        count = 0;
+    // Calculating
+    enter_key.addEventListener('click', function(){
+        console.log("enter has been selected"); // NOTE: del 
+        console.log(number_list);
 
-        // Exit
+        // End function
         return;
     });
-         
 
-};
-
-function readFunctionKeys() {
-
-};
-
-function readOperationKeys() {
+    // End function
+    return;
 };
 
 // main
-mainCalculate();
+readNumberPad();
